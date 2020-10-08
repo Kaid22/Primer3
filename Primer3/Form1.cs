2ee6b459c9f9c794b6091294cb6716d89c38a7c4
Kaid22 <avdickaid@gmail.com>
2020-10-08 13:45:40 +0200

    commit

--- Primer3/Form1.cs
+++ Primer3/Form1.cs
@@ -16,5 +16,22 @@ namespace Primer3
         {
             InitializeComponent();
         }
+        int x, y;
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            int x0 = Math.Min(x, e.X);
+            int y0 = Math.Min(y, e.Y);
+            int a = Math.Abs(e.X - x);
+            int b = Math.Abs(e.Y - y);
+            Graphics g = CreateGraphics();
+            g.DrawRectangle(Pens.Black, x0, y0, a, b);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            x = e.X;
+            y = e.Y;
+        }
     }
 }
